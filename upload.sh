da1142d13f8a7bab820946a7150fcb9a7c8257fa
jiangjiawen <jiawen.jiang@bkjk.com>
2018-03-23 10:06:23 +0800

    2

--- upload.sh
+++ upload.sh
@@ -4,12 +4,12 @@ git stash
 git pull origin master --tags
 git stash pop
 
-VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
+VersionString=`grep -E 's.version.*=' B_Category.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
+LineNumber=`grep -nE 's.version.*=' B_Category.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" B_Category.podspec
 
 echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
