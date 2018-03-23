0dcbd3a00f2d615318fdfa3dfe800bff6f624726
jiangjiawen <jiawen.jiang@bkjk.com>
2018-03-23 10:03:06 +0800

    1

--- upload.sh
+++ upload.sh
@@ -4,12 +4,12 @@ git stash
 git pull origin master --tags
 git stash pop
 
-VersionString=`grep -E 's.version.*=' testSpec.podspec`
+VersionString=`grep -E 's.version.*=' __ProjectName__.podspec`
 VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
 
 NewVersionNumber=$(($VersionNumber + 1))
-LineNumber=`grep -nE 's.version.*=' testSpec.podspec | cut -d : -f1`
-sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" testSpec.podspec
+LineNumber=`grep -nE 's.version.*=' __ProjectName__.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" __ProjectName__.podspec
 
 echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
 
@@ -17,5 +17,5 @@ git add .
 git commit -am ${NewVersionNumber}
 git tag ${NewVersionNumber}
 git push origin master --tags
-cd ~/.cocoapods/repos/CasaPasswordRepositoryStore && git pull origin master && cd - && pod repo push CasaPasswordRepositoryStore B_Category.podspec --verbose --allow-warnings --use-libraries
+cd ~/.cocoapods/repos/testSpec && git pull origin master && cd - && pod repo push testSpec B_Category.podspec --verbose --allow-warnings --use-libraries
 
